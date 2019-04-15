22c99581235462419f742154935e1d8436ff3a7a
zach calcagno <zcalcagno2480@jh-mba158-02.local>
2019-04-15 14:53:33 -0500

    Initial Commit

---
+++ pong/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  pong
+//
+//  Created by zach calcagno on 4/15/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
